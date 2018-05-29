37391f039c078aef366ee764ff351a8be9c2691f
bukabu <bukabuweb@gmail.com>
2018-05-29 21:09:53 +0100

    updated feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,18 +1,17 @@
-/* feedreader.js
- */
+/* feedreader.js*/
 
 /* All tests are within the $() function,
  * since some of these tests may require DOM elements. It is to make sure they don't run until the DOM is ready.
  */
-$(function() {
+$(function () {
     /* the first test suite -  all about the RSS feeds definitions
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', function () {
         /* the first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty.
          */
-        it('are defined', function() {
+        it('are defined', function () {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -25,11 +24,11 @@ $(function() {
         it('URL are defined', function () {
 
             allFeeds.forEach(function (feed) {
-            expect(feed.url).toBeDefined();
-            expect(feed.url).not.toBe(0);
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe(0);
             });
 
-          });
+        });
 
         /*a test that loops through each feed
          * and ensures it has a name defined
@@ -38,10 +37,10 @@ $(function() {
          it('Names are defined', function () {
 
            allFeeds.forEach(function (feed) {
-             expect(feed.name).toBeDefined();
-             expect(feed.name).not.toBe(0);
+               expect(feed.name).toBeDefined();
+               expect(feed.name).not.toBe(0);
             });
-        });
+         });
     });
 
 
@@ -51,13 +50,13 @@ $(function() {
         /* a test that ensures the menu element is
          * hidden by default.
           */
-         const menu = document.getElementsByClassName('menu-hidden');
+        const menu = document.getElementsByClassName('menu-hidden');
 
 
-         it(' element is hidden by default', function () {
+        it(' element is hidden by default', function () {
             expect(menu.length).toBe(1);
 
-         });
+        });
 
 
 
@@ -65,28 +64,27 @@ $(function() {
           * visibility when the menu icon is clicked.
           */
 
-          it(' changes visibility', function () {
+        it(' changes visibility', function () {
             const menuIcon = document.querySelector('.menu-icon-link');
 
             menuIcon.click();
             expect(menu.length).toBe(0);
             menuIcon.click();
-            expect(menu.length).toBe(1);
+           expect(menu.length).toBe(1);
 
-
-          });
-
+        });
 
     });
 
 
     /* a test suite named "Initial Entries" */
-    describe('Initial Entries', function() {
+    describe('Initial Entries', function () {
 
-      beforeEach(function(done) {
-        loadFeed(0, function() {
+      beforeEach(function (done) {
+        loadFeed(0, function () {
           done();
         });
+
       });
 
         /* a test that ensures when the loadFeed
@@ -94,12 +92,12 @@ $(function() {
          * a single .entry element within the .feed container.
          */
 
-         it(' have at least one feed in the container', function (done) {
+        it(' have at least one feed in the container', function (done) {
            const container = document.querySelector('.feed').innerHTML;
-            expect(container).not.toBe('');
-            done();
+           expect(container).not.toBe('');
+           done();
 
-         });
+        });
 
     });
 
